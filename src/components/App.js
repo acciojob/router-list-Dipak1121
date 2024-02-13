932f2d7781d0e4b9c7c635d75aa83dc1c824d487
Dipak1121 <dipaks9604@gmail.com>
2024-02-13 12:42:24 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 
 import React from "react";
 import './../styles/App.css';
+import Item1 from "./Item1";
+import Item2 from "./Item2";
+import Item3 from "./Item3";
+import List from "./List";
+import { useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
+
+  let navigate = useNavigate();
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        
+        <Routes>
+          <Route path="/" element={<List/>}/>
+          <Route path="/items/1" element={<Item1/>} />
+          <Route path="/items/2" element={<Item2/>} />
+          <Route path="/items/3" element={<Item3/>} />
+        </Routes>
     </div>
   )
 }
